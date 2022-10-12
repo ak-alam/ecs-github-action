4ab634af654ab5ac9cd0d00eea7bbc1618c31203
ak-alam <alam.akbar1989@gmail.com>
2022-10-12 23:17:00 +0500

    added deployment

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
-    return "Flask Server Running"
+    return "Flask Changes ECS Running"
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello From AWS ECS!!"
+    return "Hello ECS and Github Action"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
