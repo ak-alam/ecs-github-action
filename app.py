a47b6163bb29d55739523e094db4cb8fe48dffc7
ak-alam <alam.akbar1989@gmail.com>
2022-10-12 05:38:24 +0500

    yml file correction

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def welcome():
     
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello From AWS ECS!!!"
+    return "Hello From AWS ECS!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
